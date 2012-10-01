b63d3fc10aeac6ebbd11e89d9fef9b0c8cffcc5a
heyabo <helloyabo@gmail.com>
2012-10-01 00:48:48 -0700

    an

--- test.cpp
+++ test.cpp
@@ -4,9 +4,8 @@ int main()
 {
 	cout<<"welcom ligexiao"<<endl;
 	string str("he");
-	str=str+"yabo";
+	str=str+"yabohello";
 	int i=10;
-	cout<<"the number is:"<<i<<endl;
 	int j=i+10;
 	cout<<"the number is:"<<j<<endl;
 	cout<<str<<endl;
